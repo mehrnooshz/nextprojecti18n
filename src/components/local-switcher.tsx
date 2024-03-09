bda69cfbcafdf016c928c46d8cdd4bd1746984b3
Mehrnoosh <eng.mehrnoosh.mousavi@gmail.com>
2024-03-09 12:10:39 +0330

    fixed swticher local

--- src/components/local-switcher.tsx
+++ src/components/local-switcher.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { useLocale } from "next-intl";
-import { useRouter } from "next/navigation";
+import { createSharedPathnamesNavigation } from "next-intl/navigation";
+const locales = ["en", "de"] as const;
+const { useRouter, usePathname } = createSharedPathnamesNavigation({ locales });
 import { ChangeEvent, useTransition } from "react";
 
 export default function LocalSwitcher() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
+  const path = usePathname();
   const localActive = useLocale();
 
   const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const nextLocale = e.target.value;
+
     startTransition(() => {
-      router.replace(`/${nextLocale}`);
+      router.replace(path, { locale: nextLocale });
     });
   };
   return (
